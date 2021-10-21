b92a079631ca14b71e1649319ef9dfad8c591386
Federico Vazquez <fvazquez@abstractsolutions.com.ar>
2021-10-21 15:00:04 -0300

    Feat 1.1 Agregamos Role y User

---
+++ src/main/java/com/AppConLoggin/springbootApp/entities/Role.java
@@ -0,0 +1,27 @@
+package com.AppConLoggin.springbootApp.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Role implements Serializable {
+
+  private static final long SerialVersionUID = 2969524610059270447L;
+
+  @Id
+  @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+  @GenericGenerator(name="native",strategy="native")
+  private Long id;
+
+  @Column
+  private String name;
+
+  @Column
+  private String description;
+
+}
---
+++ src/main/java/com/AppConLoggin/springbootApp/entities/User.java
@@ -0,0 +1,52 @@
+package com.AppConLoggin.springbootApp.entities;
+
+
+import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Transient;
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Entity
+public class User implements Serializable {
+
+  private static final long serialVersionUID = -6833167247953613395L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+  @GenericGenerator(name = "native", strategy = "native")
+  private Long id;
+
+  @Column
+  private String firstName;
+  @Column
+  private String lastName;
+  @Column(unique = true)
+  private String email;
+  @Column(unique = true)
+  private String username;
+  @Column
+  private String password;
+
+  @Transient
+  private String confirmPassword;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(name = "user_roles"
+      , joinColumns = @JoinColumn(name = "user_id")
+      , inverseJoinColumns = @JoinColumn(name = "role_id"))
+  private Set roles;
+
+  public User() {
+  }
+
+}
